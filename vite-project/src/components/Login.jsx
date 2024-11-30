3fec7bf74051fe597ed4d6689915b3b5623e0db5
alvinholuis <alvinholuis222@gmail.com>
2024-11-30 14:06:58 -0300

    att inputs login

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -2,18 +2,29 @@ function Login(){
     return(
     <body>
         <h1>LOGIN</h1>
-        <div className="forms">
-            <select className="select-cargo" name="cargo" id="select-cargo">
-                <option value="">CARGO</option>
-                <option value="teste1">JAKSHDKAS</option>
-                <option value="teste2">asdasd</option>
-                <option value="teste3">dfgdfg</option>
-                <option value="teste4">asdasdg</option>
-            </select>
+        <form className="forms">
 
-            <input className="email" type="email" name="" id="" />
-            <input className="senha" type="password" name="" id="" />
-        </div>
+            <div class="input-floating">
+                <select className="select-cargo" name="cargo" id="select-cargo">
+                    <option value="">CARGO</option>
+                    <option value="teste1">JAKSHDKAS</option>
+                    <option value="teste2">asdasd</option>
+                    <option value="teste3">dfgdfg</option>
+                    <option value="teste4">asdasdg</option>
+                </select>
+            </div>
+
+            <div class="input-floating">
+                <input type="email" id="input" placeholder=" " required/>
+                <label for="input">Email</label>
+            </div>
+
+            <div class="input-floating">
+                <input type="password" id="input" placeholder=" " required/>
+                <label for="input">Senha</label>
+            </div>
+
+        </form>
     </body>
     )
 }
